817a8deefd67cb08167f76f5f8f6e28ed66aa0bd
Sebastian Sánchez <117509425+Minorey@users.noreply.github.com>
2024-04-02 23:16:10 -0500

    Mino comit

---
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.loc.newsapp.util
+
+object Constants {
+
+    const val  USER_SETTINGS = "userSettings"
+
+    const val APP_ENTRY = "app_entry"
+
+    const val API_KEY = "404dc31edb754125b5771ee1b252f0f2"
+}
